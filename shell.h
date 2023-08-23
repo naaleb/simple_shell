f4c57dbe776569c3b7dc67561185be81602d1918
Almond335 <abeboralmond@gmail.com>
2023-08-23 12:45:05 -0700

    Header file

--- shell.h
+++ shell.h
@@ -1,14 +1,20 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <string.h>
-#include <errno.h>
+#include <sys/wait.h>
 
-void shell_prompt(char **av, char **env);
+#define MAX_COMMAND_LENGTH 1024
 
-#endif
+/* Function prototypes */
+char *read_command(void);
+char **split_command(char *command);
+void execute_command(char **args);
+void free_args(char **args);
+
+#endif /* MAIN_H */
+
